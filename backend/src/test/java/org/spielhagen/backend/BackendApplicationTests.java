d77308c241bb02763bc6debc6553942da46162fe
Sebastian Spielhagen <s.spielhagen@me.com>
2023-12-12 12:28:52 +0100

    delete some files

---
+++ backend/src/test/java/org/spielhagen/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.spielhagen.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
